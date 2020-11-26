8dcbd8526fa3bbcac9e93402b1e8eb4310b41c09
Will Jiang <20015jjw@gmail.com>
2020-11-26 02:11:20 -0600

    Initial Commit: 1. Made a working live wallpaper 2. Made the live wallpaper changes color as the dark mode is toggled on and off

---
+++ app/src/main/java/com/example/darkmodeautochanginglivewallpaper/AutoChangingLiveWallpaperService.kt
@@ -0,0 +1,76 @@
+package com.example.darkmodeautochanginglivewallpaper
+
+import android.content.res.Configuration
+import android.service.wallpaper.WallpaperService
+import android.view.SurfaceHolder
+import android.widget.Toast
+
+
+class AutoChangingLiveWallpaperService : WallpaperService() {
+
+    private lateinit var mEngine: AutoChangingWallpaperEngine
+
+    override fun onCreateEngine(): Engine {
+        mEngine = AutoChangingWallpaperEngine()
+        update(resources.configuration)
+        return mEngine
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        update(newConfig)
+    }
+
+    private fun update(newConfig: Configuration) {
+        val isDarkMode = isDarkMode(newConfig)
+        Toast.makeText(this, if (isDarkMode) "yes" else "no", Toast.LENGTH_SHORT).show()
+        mEngine.update(isDarkMode)
+    }
+
+    private fun isDarkMode(newConfig: Configuration): Boolean {
+        return newConfig.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES
+    }
+
+    private inner class AutoChangingWallpaperEngine : WallpaperService.Engine(),
+        SurfaceHolder.Callback {
+        private var mSurfaceHolder: SurfaceHolder? = null
+        private var mInitialized = false
+        private var mIsDarkMode = false
+
+        override fun onCreate(surfaceHolder: SurfaceHolder?) {
+            super.onCreate(surfaceHolder)
+            mSurfaceHolder = surfaceHolder
+            surfaceHolder?.addCallback(this)
+        }
+
+        override fun surfaceCreated(holder: SurfaceHolder) {
+            mInitialized = true
+            draw(holder)
+        }
+
+        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+            draw(holder)
+        }
+
+        override fun surfaceDestroyed(holder: SurfaceHolder) {
+        }
+
+        fun update(isDarkMode: Boolean) {
+            mIsDarkMode = isDarkMode
+            draw(surfaceHolder)
+        }
+
+        private fun draw(holder: SurfaceHolder) {
+            if (mInitialized) {
+                val canvas = holder.lockCanvas()
+                if (mIsDarkMode) {
+                    canvas.drawRGB(0, 0, 0)
+                } else {
+                    canvas.drawRGB(0, 125, 125)
+                }
+                holder.unlockCanvasAndPost(canvas)
+            }
+        }
+    }
+
+}
